acdbac435fa3be426a8ebc8e0173f0ef6a5d0f2f
Ing Danier Camacho Melgarejo <dacame95@gmail.com>
2022-01-22 11:13:08 -0500

    mi primera prueba git

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,142 @@
+<aside class="main-sidebar">
+
+	 <section class="sidebar">
+
+		<ul class="sidebar-menu">
+
+			<li class="active">
+
+				<a href="inicio">
+
+					<i class="fa fa-home"></i>
+					<span>Inicio</span>
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="usuarios">
+
+					<i class="fa fa-user"></i>
+					<span>Usuarios</span>
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="comite">
+
+					<i class="fa fa-users"></i>
+					<span>Comite base </span>
+
+				</a>
+
+			</li>
+			<li>
+
+				<a href="juventud">
+
+					<i class="fa fa-users"></i>
+					<span>Comite Juventudes</span>
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="categorias">
+
+					<i class="fa fa-building"></i>
+					<span>Empresa</span>
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="productos">
+
+					<i class="fa fa-users"></i>
+					<span>Trabajadores</span>
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="clientes">
+
+					<i class="fa fa-users"></i>
+					<span>Clientes</span>
+
+				</a>
+
+			</li>
+
+			<li class="treeview">
+
+				<a href="#">
+
+					<i class="fa fa-list-ul"></i>
+					
+					<span>Ventas</span>
+					
+					<span class="pull-right-container">
+					
+						<i class="fa fa-angle-left pull-right"></i>
+
+					</span>
+
+				</a>
+
+				<ul class="treeview-menu">
+					
+					<li>
+
+						<a href="ventas">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Administrar ventas</span>
+
+						</a>
+
+					</li>
+
+					<li>
+
+						<a href="crear-venta">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Crear venta</span>
+
+						</a>
+
+					</li>
+
+					<li>
+
+						<a href="reportes">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Reporte de ventas</span>
+
+						</a>
+
+					</li>
+
+				</ul>
+
+			</li>
+
+		</ul>
+
+	 </section>
+
+</aside>
